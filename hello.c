9535c48e7aaaa23b8c1c9b0fdb7945daaa0303fc
wlzhit <wlzhit@gmail.com>
2019-12-01 11:29:13 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,7 +3,8 @@ using namespace std;
 
 int main()
 {
-  cout << "Hello world!\n" << endl;
+  cout << "Hello world!" << endl;
   cout << "This is the first test for using github!" << endl;
+  cout << "Added for test update!" << endl;
   return 1;
 }
